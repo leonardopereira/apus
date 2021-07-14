fc2d0bc2763a2768a29e9359b2ac6b47b19d384c
Leonardo Freitas Pereira <pereira.leonardo@cwi.com.br>
2021-07-14 15:58:55 -0300

    Alterando arquivos pro projeto rodar

--- src/main/java/br/com/cwi/apus/repository/OrderRepository.java
+++ src/main/java/br/com/cwi/apus/repository/OrderRepository.java
@@ -1,7 +1,4 @@
 package br.com.cwi.apus.repository;
 
-import br.com.cwi.apus.domain.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository {
 }
